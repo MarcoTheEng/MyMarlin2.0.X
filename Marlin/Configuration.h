4d057aaf7343bf49f7cf7961953de6c33b9f42c5
MarcoTheEng <farmidaler123@gmail.com>
2021-07-29 17:52:48 +1000

    Preheat Values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1503,10 +1503,15 @@
 #define PREHEAT_1_TEMP_BED     45
 #define PREHEAT_1_FAN_SPEED     255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
+#define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    0
+#define PREHEAT_2_TEMP_BED    75
 #define PREHEAT_2_FAN_SPEED     255 // Value from 0 to 255
+
+#define PREHEAT_3_LABEL       "ABS"
+#define PREHEAT_3_TEMP_HOTEND 240
+#define PREHEAT_3_TEMP_BED    90
+#define PREHEAT_3_FAN_SPEED     255 // Value from 0 to 255
 
 /**
  * Nozzle Park
