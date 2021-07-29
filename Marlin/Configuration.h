e088a95b986da496cc8fd05e3993209b56d251d1
MarcoTheEng <farmidaler123@gmail.com>
2021-07-29 17:49:44 +1000

    FirmwareAuthor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Solid Snake, Ender 5 SkrV1.4Turbo)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
